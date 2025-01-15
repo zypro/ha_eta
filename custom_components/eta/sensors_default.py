46e44759fc8888188448b101fc471996bafe053c
Michael Schmid <zypro@users.noreply.github.com>
2025-01-15 10:30:56 +0100

    Update sensors_default.py to new units

--- custom_components/eta/sensors_default.py
+++ custom_components/eta/sensors_default.py
@@ -5,45 +5,45 @@
 @factor is mandatory
 """
 from homeassistant.components.sensor import SensorStateClass, SensorDeviceClass
-from homeassistant.const import TEMP_CELSIUS, POWER_KILO_WATT, MASS_KILOGRAMS, ENERGY_KILO_WATT_HOUR
+from homeassistant.const import UnitOfTemperature, UnitOfPower, UnitOfMass, UnitOfEnergy
 
 SENSORS_DEFAULT = [
     {
         "uri": "/120/10601/0/0/12197",
-        "unit": TEMP_CELSIUS
+        "unit": UnitOfTemperature.CELSIUS
     },
     {
         "uri": "/40/10021/0/0/12077",
-        "unit": POWER_KILO_WATT
+        "unit": UnitOfPower.KILO_WATT
     },
     {
         "uri": "/40/10021/0/0/12006",
-        "unit": TEMP_CELSIUS
+        "unit": UnitOfTemperature.CELSIUS
     },
     {
         "uri": "/40/10021/0/11109/0",
-        "unit": TEMP_CELSIUS
+        "unit": UnitOfTemperature.CELSIUS
     },
     {
         "uri": "/40/10021/0/11110/0",
-        "unit": TEMP_CELSIUS
+        "unit": UnitOfTemperature.CELSIUS
     },
     {
         "uri": "/120/10101/0/11125/2121",
-        "unit": TEMP_CELSIUS
+        "unit": UnitOfTemperature.CELSIUS
     },
     {
         "uri": "/40/10201/0/0/12015",
-        "unit": MASS_KILOGRAMS
+        "unit": UnitOfMass.KILOGRAMS
     },
     {
         "uri": "/40/10021/0/0/12016",
-        "unit": MASS_KILOGRAMS
+        "unit": UnitOfMass.KILOGRAMS
     },
     {
         "name": "Gesamt Energieverbrauch",
         "uri": "/40/10021/0/0/12016",
-        "unit": ENERGY_KILO_WATT_HOUR,
+        "unit": UnitOfEnergy.KILO_WATT_HOUR,
         "device_class": SensorDeviceClass.ENERGY,
         "state_class": SensorStateClass.TOTAL_INCREASING,
         "factor": 4.8
